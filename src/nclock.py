720ee8656dea127f11e254496687dc83a4b1c4d5
Daniel Poelzleithner <git@poelzi.org>
2010-08-04 01:50:05 +0200

    arguments and keyboard interrupt support
    exit program when keyboard interrupt is pressed (ctrl + c)
    started command line arguments. don't start in fullscreen mode by default

--- src/nclock.py
+++ src/nclock.py
@@ -3,12 +3,30 @@
 from PyQt4 import QtCore
 from PyQt4.QtGui import QApplication, QMessageBox
 from ui.clockstub import  MainWindow
+from optparse import OptionParser
+
+# catch keyboard interrupt
+import signal
+signal.signal(signal.SIGINT, signal.SIG_DFL)
 
 if __name__ == "__main__":
     import sys
+
+    parser = OptionParser()
+    parser.add_option("-f", "--fullscreen", dest="fullscreen",
+                      help="start in fullscreen mode", action="store_true")
+
+    (options, args) = parser.parse_args()
+
+
     app = QApplication(sys.argv)
+    app.setQuitOnLastWindowClosed(True)
     ui = MainWindow()
-    
-    ui.showFullScreen()
+
+    if options.fullscreen:
+        ui.showFullScreen()
+    else:
+        ui.show()
+
     sys.exit(app.exec_())
 #
